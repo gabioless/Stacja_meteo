b980970ab7afeea4ff54af448300228af0a37e20
gabioless <gabriel.oginski@gmail.com>
2017-03-14 15:26:12 +0100

    1 wruczenie plikow
    polaczenie prawidlowe workspace

---
+++ Stacja/main.c
@@ -0,0 +1,27 @@
+/*
+ * main.c
+ *
+ *  Created on: 14.03.2017
+ *      Author: gabog
+ */
+
+//dolaczania naglowek
+#include <avr/io.h>
+
+//zmienne globalne
+
+//funckja glowna
+int main(void){
+
+	//inicjalizacja sprzetu
+
+
+
+	//petla nieskonczona
+	while(1){
+
+	}
+
+	return 0;
+}
+
